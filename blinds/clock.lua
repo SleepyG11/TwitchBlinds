561df4077b35b510871091b82a1974c6ec4c1a57
makatymba2001 <makatymba2001@yandex.ru>
2024-07-29 02:53:12 +0300

    The Clock rebalance

--- blinds/clock.lua
+++ blinds/clock.lua
@@ -1,6 +1,7 @@
-local TIME_DELAY = 4 -- In seconds
-local MULT_INCREMENT = 0.25
--- Total time to full grow: 4 * 4 * 6 = 92 seconds
+local MAX_SIZE = 8
+local TIME_DELAY = 1 -- In seconds
+local MULT_INCREMENT = 0.06
+-- Total time to full grow: 1 / 0.06 * (8 - 2) = 100 seconds
 
 local tw_blind = SMODS.Blind {
     key = register_twitch_blind('clock', false),
@@ -23,20 +24,21 @@ local tw_blind = SMODS.Blind {
 
 local timeout = TIME_DELAY
 
-function blind_clock_increment_mult(mult)
-    if mult >= 8 then return 8 end
-    return mult + MULT_INCREMENT
+function blind_clock_increment_chips(current_chips, base_chips)
+    local mult = current_chips / base_chips
+    if mult < MAX_SIZE then G.GAME.blind:wiggle() end
+    return base_chips * math.min(MAX_SIZE, mult + MULT_INCREMENT)
 end
 
 function blind_clock_request_increment_mult(dt)
+    if G.SETTINGS.paused then return end
     timeout = timeout - dt
     if timeout <= 0 then
         timeout = timeout + TIME_DELAY
         if G.GAME and G.GAME.blind and G.GAME.blind.name == get_twitch_blind_key('clock') and G.GAME.round_resets.blind_states.Boss == 'Current' then
-            G.GAME.blind:wiggle()
-            G.GAME.blind.mult = blind_clock_increment_mult(G.GAME.blind.mult)
-            G.GAME.blind.chips = get_blind_amount(G.GAME.round_resets.ante) * G.GAME.blind.mult *
-                G.GAME.starting_params.ante_scaling
+            -- TODO: need to fix a problem with no chips saving
+            G.GAME.blind.chips = blind_clock_increment_chips(G.GAME.blind.chips,
+                get_blind_amount(G.GAME.round_resets.ante) * G.GAME.starting_params.ante_scaling)
             G.GAME.blind.chip_text = number_format(G.GAME.blind.chips)
             G.GAME.blind:set_text()
         end
@@ -45,10 +47,9 @@ end
 
 function tw_blind:set_blind(reset, silent)
     if reset then return end
-    self.mult = 2
     timeout = TIME_DELAY * 2 -- reset + preparation time + animations time
 end
 
 function tw_blind:defeat()
-    self.mult = 2
+
 end

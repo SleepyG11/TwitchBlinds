43dc22662e3e940e67f6f784c7ea894b7e7599ac
makatymba2001 <makatymba2001@yandex.ru>
2024-08-28 16:50:30 +0300

    Food jokers from mods for The Expiration Date

--- blinds/expiration.lua
+++ blinds/expiration.lua
@@ -1,21 +1,65 @@
-local FOOL_JOKERS = {
-	["Gros Michel"] = true,
-	["Egg"] = true,
-	["Ice Cream"] = true,
-	["Cavendish"] = true,
-	["Turtle Bean"] = true,
-	["Diet Cola"] = true,
-	["Popcorn"] = true,
-	["Ramen"] = true,
-	["Seltzer"] = true,
+local FOOD_JOKERS_KEYS = {
+	["j_gros_michel"] = true,
+	["j_egg"] = true,
+	["j_ice_cream"] = true,
+	["j_cavendish"] = true,
+	["j_turtle_bean"] = true,
+	["j_diet_cola"] = true,
+	["j_popcorn"] = true,
+	["j_ramen"] = true,
+	["j_selzer"] = true,
+	["j_cry_pickle"] = true,
+	["j_cry_chili_pepper"] = true,
+	["j_cry_oldcandy"] = true,
+	["j_cry_caramel"] = true,
+	["j_cry_foodm"] = true,
+	["j_jank_cut_the_cheese"] = true,
+	["j_cafeg"] = true,
+	["j_cherry"] = true,
+	["j_evo_full_sugar_cola"] = true,
+	["j_bunc_starfruit"] = true,
+	["j_bunc_fondue"] = true,
+	["j_kcva_fortunecookie"] = true,
+	["j_kcva_swiss"] = true,
+	["j_olab_taliaferro"] = true,
+	["j_olab_royal_gala"] = true,
+	["j_olab_fine_wine"] = true,
+	["j_olab_mystery_soda"] = true,
+	["j_olab_popcorn_bag"] = true,
+	["j_snow_turkey_dinner"] = true,
+	["j_ssj_coffee"] = true,
+	["j_twewy_candleService"] = true,
+	["j_twewy_burningCherry"] = true,
+	["j_twewy_burningMelon"] = true,
+	["j_pape_soft_taco"] = true,
+	["j_pape_crispy_taco"] = true,
+	["j_pape_nachos"] = true,
+	["j_pape_ghost_cola"] = true,
+	["j_sdm_burger"] = true,
+	["j_sdm_pizza"] = true,
 }
+
+local FOOD_JOKERS_NAMES = {}
+
+local function create_food_jokers_table()
+	for k, v in pairs(FOOD_JOKERS_KEYS) do
+		if v and G.P_CENTERS[k] then
+			FOOD_JOKERS_NAMES[G.P_CENTERS[k].name] = true
+		end
+	end
+end
 
 local function check_is_food_jokers()
 	if not G.jokers then
 		return false
 	end
+
+	if not next(FOOD_JOKERS_NAMES) then
+		create_food_jokers_table()
+	end
+
 	for _, v in ipairs(G.jokers.cards) do
-		if FOOL_JOKERS[v.ability.name] then
+		if FOOD_JOKERS_NAMES[v.ability.name] then
 			return true
 		end
 	end
@@ -48,9 +92,14 @@ function tw_blind:set_blind(reset, silent)
 	if reset then
 		return
 	end
+
+	if not next(FOOD_JOKERS_NAMES) then
+		create_food_jokers_table()
+	end
+
 	local cards_to_remove = {}
 	for _, v in ipairs(G.jokers.cards) do
-		if FOOL_JOKERS[v.ability.name] then
+		if FOOD_JOKERS_NAMES[v.ability.name] then
 			table.insert(cards_to_remove, v)
 		end
 	end

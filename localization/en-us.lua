acc05cffb7b95b91bb890b08dbef7e66748edd19
Djynasty <55254209+OceanRamen@users.noreply.github.com>
2024-10-26 23:24:34 +0100

    Update en-us.lua

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -1,245 +1,244 @@
 return {
-	descriptions = {
-		Other = {
-			twbl_chat_booster = {
-				name = "Chat Booster",
-				text = { "Chat can interact", "inside this booster pack" },
-			},
-		},
-		Blind = {
-			bl_twbl_twitch_chat = {
-				name = "The Chat",
-				text = { "Select to end voting and", "begin a challenge from chat" },
-			},
-			bl_twbl_banana = {
-				name = "The Banana",
-				text = { "#1# in #2# chance to replace", "Joker with Gros Michel" },
-			},
-			bl_twbl_blank = {
-				name = "The Blank",
-				text = { "Does nothing?" },
-			},
-			bl_twbl_chaos = {
-				name = "The Chaos",
-				text = { "Chat can select or", "deselect cards" },
-			},
-			bl_twbl_chisel = {
-				name = "The Chisel",
-				text = { "First #1#", "does not score" },
-			},
-			bl_twbl_circus = {
-				name = "The Circus",
-				text = { "The show is", "about to begin!" },
-			},
-			bl_twbl_clock = {
-				name = "The Hourglass",
-				text = { "Hurry up!" },
-			},
-			bl_twbl_dice = {
-				name = "The Dice",
-				text = { "Chat can roll to gain 1$", "#1# in #2# chance to lose 6$", "Single-Use: roll" },
-			},
-			bl_twbl_expiration = {
-				name = "The Expiration Date",
-				text = { "All food Jokers are", "out of expiration date" },
-			},
-			bl_twbl_flashlight = {
-				name = "The Flashlight",
-				text = { "All cards are face down", "Chat can flip them" },
-			},
-			bl_twbl_greed = {
-				name = "The Greed",
-				text = { "No shop", "after this blind" },
-			},
-			bl_twbl_isaac = {
-				name = "The Voice of God",
-				text = { "To prove your love and devotion", "I require a sacrifice!" },
-			},
-			bl_twbl_jimbo = {
-				name = "The Jimbo",
-				text = { "Hey, look!", "It's a Jimbo!" },
-			},
-			bl_twbl_lock = {
-				name = "The Lock",
-				text = { "Chat can add or remove", "eternal sticker on Jokers" },
-			},
-			bl_twbl_moon = {
-				name = "The Moon",
-				text = { "Redeems Planet Merchant", "and Planet Tycoon" },
-			},
-			bl_twbl_pin = {
-				name = "The Pin",
-				text = { "Chat can pin or", "unpin Jokers" },
-			},
-			bl_twbl_precision = {
-				name = "The Precision",
-				text = { "Must select 5 cards", "to discard" },
-			},
-			bl_twbl_sparkle = {
-				name = "The Sparkle",
-				text = { "Redeems Magic Trick", "and Illusion" },
-			},
-			bl_twbl_taxes = {
-				name = "The Taxes",
-				text = { "#1# in #2# chance to", "Joker became Rental" },
-			},
-			bl_twbl_trash_can = {
-				name = "The Trash Can",
-				text = { "All scored cards are", "removed from the deck" },
-			},
-			bl_twbl_vaporation = {
-				name = "The Vaporation",
-				text = { "Current Jokers", "became Perishable" },
-			},
-			bl_twbl_eraser = {
-				name = "The Eraser",
-				text = { "Chat can select which", "Joker will be destroyed", "after boss defeat" },
-			},
-			bl_twbl_sketch = {
-				name = "The Sketch",
-				text = { "Chat can select which", "Joker will be copied", "after boss defeat" },
-			},
-			bl_twbl_nope = {
-				name = "Nope!",
-				text = { "Nope!" },
-			},
-			bl_twbl_misstock = {
-				name = "The Misstock",
-				text = { "Chat can select which", "single card type will", "appear in next shop" },
-			},
-			bl_twbl_incrementor = {
-				name = "The Incrementor",
-				text = { "Chat can start counting", "to increase blind size" },
-			},
-			bl_twbl_lucky_wheel = {
-				name = "The Lucky Wheel",
-				text = { "#1# in #2# chance to add or", "replace editions on all Jokers" },
-			},
-			bl_twbl_spiral = {
-				name = "The Time Spiral",
-				text = { "Chat can move in time" },
-			},
+    descriptions = {
+        Other = {
+            twbl_chat_booster = {
+                name = "Chat Booster",
+                text = { "The Chat can interact", "inside this booster pack!" },
+            },
+        },
+        Blind = {
+            bl_twbl_twitch_chat = {
+                name = "The Chat",
+                text = { "Select to end voting and", "begin a challenge from the Chat!" },
+            },
+            bl_twbl_banana = {
+                name = "The Banana",
+                text = { "Each Joker has a", "#1# in #2# chance to be", "replaced with Gros Michel!" },
+            },
+            bl_twbl_taxes = {
+                name = "The Taxes",
+                text = { "Each Joker has a", "#1# in #2# chance to", "become Rental!" },
+            },
+            bl_twbl_lucky_wheel = {
+                name = "The Lucky Wheel",
+                text = { "Each Joker has a", "#1# in #2# chance to", "change edition!" }
+            },
+            bl_twbl_blank = {
+                name = "The Blank",
+                text = { "Does nothing?" },
+            },
+            bl_twbl_chaos = {
+                name = "The Chaos",
+                text = { "The Chat can select and", "deselect cards!" },
+            },
+            bl_twbl_chisel = {
+                name = "The Chisel",
+                text = { "The first #1#", "does not score!" },
+            },
+            bl_twbl_circus = {
+                name = "The Circus",
+                text = { "The show is", "about to begin!" },
+            },
+            bl_twbl_clock = {
+                name = "The Hourglass",
+                text = { "Hurry up!" },
+            },
+            -- Not used
+            bl_twbl_dice = {
+                name = "The Dice",
+                text = { "The Chat can roll to gain 1$", "#1# in #2# chance to lose 6$", "Single-Use: roll" },
+            },
+            bl_twbl_expiration = {
+                name = "The Expiration Date",
+                text = { "All food Jokers are", "out-of-date!" },
+            },
+            bl_twbl_flashlight = {
+                name = "The Flashlight",
+                text = { "All cards start face down", "only the Chat can flip them!" },
+            },
+            bl_twbl_greed = {
+                name = "The Greed",
+                text = { "Skip the next shop!" },
+            },
+            bl_twbl_isaac = {
+                name = "The Voice of God",
+                text = { "To prove your love and devotion", "I require a sacrifice!" },
+            },
+            bl_twbl_jimbo = {
+                name = "The Jimbo",
+                text = { "Hey, look!", "It's a Jimbo!" },
+            },
+            bl_twbl_lock = {
+                name = "The Lock",
+                text = { "The Chat can add and remove", "eternal stickers on Jokers!" },
+            },
+            bl_twbl_moon = {
+                name = "The Moon",
+                text = { "The Chat has bestowed", "the Planet vouchers unto you!" },
+            },
+            bl_twbl_sparkle = {
+                name = "The Sparkle",
+                text = { "The Chat has bestowed", "the Magic Trick and Illusion ", "vouchers unto you!" },
+            },
+            bl_twbl_pin = {
+                name = "The Pin",
+                text = { "The Chat can pin and", "unpin Jokers!" },
+            },
+            bl_twbl_precision = {
+                name = "The Precise",
+                text = { "Discards must contain", "5 cards!" },
+            },
+            bl_twbl_trash_can = {
+                name = "The Trash Can",
+                text = { "All scored cards are", "removed from the deck" },
+            },
+            bl_twbl_vaporation = {
+                name = "The Vaporation",
+                text = { "Your current Jokers", "will become Perishable!" },
+            },
+            bl_twbl_eraser = {
+                name = "The Eraser",
+                text = { "On Blind Win: a Joker", "chosen by the Chat", "will be destroyed!" },
+            },
+            bl_twbl_sketch = {
+                name = "The Sketch",
+                text = { "On Blind Win: a Joker", "chosen by the Chat", "will be copied!" }
+            },
+            bl_twbl_nope = {
+                name = "Nope!",
+                text = { "Nope!" },
+            },
+            bl_twbl_misstock = {
+                name = "The Misstock",
+                text = { "The Chat can select which", "single card type will", "appear in next shop!" },
+            },
+            bl_twbl_incrementor = {
+                name = "The Incrementor",
+                text = { "The Chat can start counting", "to increase the blind's' size!" },
+            },
+            bl_twbl_spiral = {
+                name = "The Time Spiral",
+                text = { "The Chat can move in time..." },
+            },
+            -- Showdown
+            bl_twbl_plum_hammer = {
+                name = "Plum Hammer",
+                text = { "The Chat can add or remove", "debuff on Jokers!" },
+            },
+        },
+    },
+    misc = {
+        labels = {
+            twbl_chat_booster = "Chat Booster",
+        },
+        dictionary = {
+            twbl_settings_twitch_channel_name = "Twitch channel name",
+            twbl_settings_enter_channel_name = "Enter channel name",
+            twbl_settings_paste_name_or_url_1 = "Paste name",
+            twbl_settings_paste_name_or_url_2 = "or url",
+            twbl_settings_blind_frequency = "Blind voting frequency",
+            twbl_settings_blind_frequency_1 = "None",
+            twbl_settings_blind_frequency_2 = "Every 2 antes",
+            twbl_settings_blind_frequency_3 = "Every ante",
+            twbl_settings_blind_pool = "Blinds available for voting",
+            twbl_settings_blind_pool_1 = "Twitch Blinds",
+            twbl_settings_blind_pool_2 = "Vanilla + Mods",
+            twbl_settings_blind_pool_3 = "All",
+            twbl_settings_desc_blind_pool = {
+                "Twitch Blinds: all blinds added by this mod",
+                "Vanilla + Mods: all except blinds added by mod",
+                "All: every blind added to a game",
+            },
+            twbl_settings_delay_for_chat = "Additional time for chat actions",
+            twbl_settings_delay_for_chat_1 = "None",
+            twbl_settings_delay_for_chat_2 = "~5 seconds",
+            twbl_settings_delay_for_chat_3 = "~10 seconds",
+            twbl_settings_delay_for_chat_4 = "~15 seconds",
+            twbl_settings_delay_for_chat_5 = "~20 seconds",
+            twbl_settings_desc_delay_for_chat = {
+                "The Game will wait after some actions",
+                "to give chat some time to interact!",
+            },
+            twbl_settings_blind_pool_type = "Blinds pool type",
+            twbl_settings_blind_pool_type_1 = "Once per pool (Vanilla)",
+            twbl_settings_blind_pool_type_2 = "Once per vote",
+            twbl_settings_blind_pool_type_3 = "Random",
+            twbl_settings_desc_blind_pool_type = {
+                "Once per poll: like Vanilla, every blind is unique",
+                "Once per ante: blinds may repeat between antes",
+                "Random: same blind can appear multiple times",
+            },
+            twbl_settings_natural_chat_booster_sticker = "Enable Chat Booster stickers",
+            twbl_settings_desc_natural_chat_booster_sticker = {
+                "In Booster packs, new stickers can appear",
+                "which adds interactions for chat",
+            },
+            twbl_settings_natural_blinds = "Add Twitch Blinds in regular pool",
+            twbl_settings_desc_natural_blinds = {
+                "Blinds added by mod can appear",
+                "in the regular game, without voting",
+            },
 
-			-- Showdown
+            twbl_argument_type_Number = "number",
 
-			bl_twbl_plum_hammer = {
-				name = "Plum Hammer",
-				text = { "Chat can add or remove", "debuff on Jokers" },
-			},
-		},
-	},
-	misc = {
-		labels = {
-			twbl_chat_booster = "Chat Booster",
-		},
-		dictionary = {
-			twbl_settings_twitch_channel_name = "Twitch channel name",
-			twbl_settings_enter_channel_name = "Enter channel name",
-			twbl_settings_paste_name_or_url_1 = "Paste name",
-			twbl_settings_paste_name_or_url_2 = "or url",
-			twbl_settings_blind_frequency = "Blind voting frequency",
-			twbl_settings_blind_frequency_1 = "None",
-			twbl_settings_blind_frequency_2 = "Every 2 antes",
-			twbl_settings_blind_frequency_3 = "Every ante",
-			twbl_settings_blind_pool = "Blinds available for voting",
-			twbl_settings_blind_pool_1 = "Twitch Blinds",
-			twbl_settings_blind_pool_2 = "Vanilla + Mods",
-			twbl_settings_blind_pool_3 = "All",
-			twbl_settings_desc_blind_pool = {
-				"Twitch Blinds: all blinds added by this mod",
-				"Vanilla + Mods: all except blinds added by mod",
-				"All: every blind added to a game",
-			},
-			twbl_settings_delay_for_chat = "Additional time for chat actions",
-			twbl_settings_delay_for_chat_1 = "None",
-			twbl_settings_delay_for_chat_2 = "~5 seconds",
-			twbl_settings_delay_for_chat_3 = "~10 seconds",
-			twbl_settings_delay_for_chat_4 = "~15 seconds",
-			twbl_settings_delay_for_chat_5 = "~20 seconds",
-			twbl_settings_desc_delay_for_chat = {
-				"Game will wait after some actions",
-				"to give chat some time to interact",
-			},
-			twbl_settings_blind_pool_type = "Blinds pool type",
-			twbl_settings_blind_pool_type_1 = "Once per pool (Vanilla)",
-			twbl_settings_blind_pool_type_2 = "Once per vote",
-			twbl_settings_blind_pool_type_3 = "Random",
-			twbl_settings_desc_blind_pool_type = {
-				"Once per poll: like Vanilla, every blind is unique",
-				"Once per ante: blinds may repeat between antes",
-				"Random: same blind can appear multiple times",
-			},
-			twbl_settings_natural_chat_booster_sticker = "Enable Chat Booster sticker",
-			twbl_settings_desc_natural_chat_booster_sticker = {
-				"In Booster packs, new sticker can appear",
-				"which adds interactions for chat",
-			},
-			twbl_settings_natural_blinds = "Add Twitch Blinds in regular pool",
-			twbl_settings_desc_natural_blinds = {
-				"Blinds added by mod can appear",
-				"in regular game, without voting",
-			},
+            twbl_position_DEFAULT_singular = "item position",
+            twbl_position_Card_singular = "card position",
+            twbl_position_Joker_singular = "joker position",
+            twbl_position_Comsumeable_singular = "consumeable position",
+            twbl_position_Booster_singular = "booster pack position",
 
-			twbl_argument_type_Number = "number",
+            twbl_position_DEFAULT_plural = "items position",
+            twbl_position_Card_plural = "cards position",
+            twbl_position_Joker_plural = "jokers position",
+            twbl_position_Comsumeable_plural = "consumeables position",
+            twbl_position_Booster_plural = "booster packs position",
 
-			twbl_position_DEFAULT_singular = "item position",
-			twbl_position_Card_singular = "card position",
-			twbl_position_Joker_singular = "joker position",
-			twbl_position_Comsumeable_singular = "consumeable position",
-			twbl_position_Booster_singular = "booster pack position",
+            k_twbl_tumors_ex = "Tumors!",
+            k_twbl_banana_ex = "Banana!",
+            k_twbl_banana_qu = "Banana?",
+            k_twbl_taxes_ex = "Taxes!",
+            k_twbl_erased_ex = "Erased!",
+            k_twbl_nope_ex = "Nope!",
+            k_twbl_replaced_ex = "Replaced!",
 
-			twbl_position_DEFAULT_plural = "items position",
-			twbl_position_Card_plural = "cards position",
-			twbl_position_Joker_plural = "jokers position",
-			twbl_position_Comsumeable_plural = "consumeables position",
-			twbl_position_Booster_plural = "booster packs position",
+            k_twbl_status_connected = "Connected.",
+            k_twbl_status_connecting = "Connecting...",
+            k_twbl_status_disconnected = "Disconnected...",
+            k_twbl_status_no_channel_name = "No channel!",
+            k_twbl_status_unknown = "...",
 
-			k_twbl_tumors_ex = "Tumors!",
-			k_twbl_banana_ex = "Banana!",
-			k_twbl_banana_qu = "Banana?",
-			k_twbl_taxes_ex = "Taxes!",
-			k_twbl_erased_ex = "Erased!",
-			k_twbl_nope_ex = "Nope!",
-			k_twbl_replaced_ex = "Replaced!",
+            k_twbl_vote_ex = "Vote!",
+            k_twbl_toggle_ex = "Toggle!",
+            k_twbl_interact_ex = "Interact!",
+            k_twbl_flip_ex = "Flip!",
+            k_twbl_lock_ex = "Lock!",
+            k_twbl_pin_ex = "Pin!",
+            k_twbl_select_ex = "Select!",
+            k_twbl_erase_ex = "Erase!",
+            k_twbl_sketch_ex = "Sketch!",
+            k_twbl_count_ex = "Count!",
+            k_twbl_reset_ex = "Reset!",
 
-			k_twbl_status_connected = "Connected.",
-			k_twbl_status_connecting = "Connecting...",
-			k_twbl_status_disconnected = "Disconnected...",
-			k_twbl_status_no_channel_name = "No channel!",
-			k_twbl_status_unknown = "...",
+            k_twbl_panel_toggle_default = "Trigger the boss blind's effect",
+            k_twbl_panel_toggle_flashlight = "Flip a card in hand (twice per chatter)",
+            k_twbl_panel_toggle_pin = "(Un)pin Joker (once per chatter)",
+            k_twbl_panel_toggle_lock = "Add/remove Eternal sticker on Joker (once per chatter)",
+            k_twbl_panel_toggle_chaos = "(De)select card in hand (multiple chatter)",
+            k_twbl_panel_toggle_eraser = "Select a Joker for deletion (once per chatter)",
+            k_twbl_panel_toggle_sketch = "Select a Joker for copying (once per chatter)",
+            k_twbl_panel_toggle_nope = "Nope! (multiple Nope!)",
+            k_twbl_panel_toggle_incrementor = "Keep counting! (multiple chatter)",
+            k_twbl_panel_toggle_plum_hammer = "Add/remove debuff on Joker (once per chatter)",
 
-			k_twbl_vote_ex = "Vote!",
-			k_twbl_toggle_ex = "Toggle!",
-			k_twbl_interact_ex = "Interact!",
-			k_twbl_flip_ex = "Flip!",
-			k_twbl_lock_ex = "Lock!",
-			k_twbl_pin_ex = "Pin!",
-			k_twbl_select_ex = "Select!",
-			k_twbl_erase_ex = "Erase!",
-			k_twbl_sketch_ex = "Sketch!",
-			k_twbl_count_ex = "Count!",
-			k_twbl_reset_ex = "Reset!",
+            k_twbl_panel_toggle_chat_booster_consumeable = "Select a target for consumable (once per chatter)",
+            k_twbl_panel_toggle_chat_booster_celestial = "Select a poker hand to downgrade (once per chatter)",
 
-			k_twbl_panel_toggle_default = "Trigger boss blind's effect",
-			k_twbl_panel_toggle_flashlight = "Flip a card in hand (twice per user)",
-			k_twbl_panel_toggle_pin = "(Un)pin Joker (once per user)",
-			k_twbl_panel_toggle_lock = "Add/remove Eternal sticker on Joker (once per user)",
-			k_twbl_panel_toggle_chaos = "(De)select card in hand (multiple use)",
-			k_twbl_panel_toggle_eraser = "Select a Joker for deletion (once per user)",
-			k_twbl_panel_toggle_sketch = "Select a Joker for copying (once per user)",
-			k_twbl_panel_toggle_nope = "Nope! (multiple Nope!)",
-			k_twbl_panel_toggle_incrementor = "Keep counting! (multiple use)",
-			k_twbl_panel_toggle_plum_hammer = "Add/remove debuff on Joker (once per user)",
+            k_twbl_spiral_p_1 = "+1 Ante",
+            k_twbl_spiral_p_2 = "+2 Antes",
+            k_twbl_spiral_m_1 = "-1 Ante",
+            k_twbl_spiral_m_2 = "-2 Antes",
 
-			k_twbl_panel_toggle_chat_booster_consumeable = "Select a target for consumable (once per user)",
-			k_twbl_panel_toggle_chat_booster_celestial = "Select a poker hand to downgrade (once per user)",
-
-			k_twbl_spiral_p_1 = "+1 Ante",
-			k_twbl_spiral_p_2 = "+2 Antes",
-			k_twbl_spiral_m_1 = "-1 Ante",
-			k_twbl_spiral_m_2 = "-2 Antes",
-
-			k_twbl_waiting_for_chat = "Waiting for chat...",
-		},
-	},
+            k_twbl_waiting_for_chat = "Waiting for chat...",
+        },
+    },
 }
